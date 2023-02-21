ec07361706632139bb7a54b144ff7bc3a0dbb4bc
unknown <ibr.s.ahmat@gmail.com>
2023-02-21 18:05:58 +0300

    Fix an errors

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -232,16 +232,21 @@ export default function Home() {
 
     if (liquidityTab) {
       return (
-        <>
+        <div>
           <div className={styles.description}>
-            You have: <br />
-            {utils.formatEther(cdBalance)} Crypto Dev Tokens.
+            You have:
             <br />
-            {utils.formatEther(ethBalance)} Ether.
+            {/* Convert the BigNumber to string using the formatEther function from ethers.js */}
+            {utils.formatEther(cdBalance)} Crypto Dev Tokens
             <br />
-            {utils.formatEther(lpBalance)} Crypto Dev LP Tokens.
+            {utils.formatEther(ethBalance)} Ether
+            <br />
+            {utils.formatEther(lpBalance)} Crypto Dev LP tokens
           </div>
           <div>
+            {/* If reserved CD is zero, render the state for liquidity zero where we ask the user
+            how much initial liquidity he wants to add else just render the state where liquidity is not zero and
+            we calculate based on the `Eth` amount specified by the user how much `CD` tokens can be added */}
             {utils.parseEther(reservedCD.toString()).eq(zero) ? (
               <div>
                 <input
@@ -252,15 +257,15 @@ export default function Home() {
                 />
                 <input
                   type="number"
-                  placeholder="Amount of CryptoDev Tokens"
+                  placeholder="Amount of CryptoDev tokens"
                   onChange={(e) =>
                     setAddCDTokens(BigNumber.from(utils.parseEther(e.target.value || "0")))
                   }
                   className={styles.input}
                 />
                 <button
-                  className={styles.button}
-                  onClick={() => _addLiquidity()}
+                  className={styles.button1}
+                  onClick={_addLiquidity}
                 >
                   Add
                 </button>
@@ -272,23 +277,25 @@ export default function Home() {
                   placeholder="Amount of Ether"
                   onChange={async (e) => {
                     setAddEther(e.target.value || "0");
-
+                    // calculate the number of CD tokens that
+                    // can be added given  `e.target.value` amount of Eth
                     const _addCDTokens = await calculateCD(
                       e.target.value || "0",
                       etherBalanceContract,
                       reservedCD,
                     );
-
                     setAddCDTokens(_addCDTokens);
                   }}
                   className={styles.input}
                 />
                 <div className={styles.inputDiv}>
-                  {`You will need ${utils.formatEther(addCDTokens)} Crypto Dev Tokens`}
+                  {/* Convert the BigNumber to string using the formatEther function from ethers.js */}
+                  {`You will need ${utils.formatEther(addCDTokens)} Crypto Dev
+                  Tokens`}
                 </div>
                 <button
                   className={styles.button1}
-                  onClick={() => _addLiquidity()}
+                  onClick={_addLiquidity}
                 >
                   Add
                 </button>
@@ -300,34 +307,36 @@ export default function Home() {
                 placeholder="Amount of LP Tokens"
                 onChange={async (e) => {
                   setRemoveLPTokens(e.target.value || "0");
-
+                  // Calculate the amount of Ether and CD tokens that the user would receive
+                  // After he removes `e.target.value` amount of `LP` tokens
                   await _getTokensAfterRemove(e.target.value || "0");
                 }}
                 className={styles.input}
               />
               <div className={styles.inputDiv}>
-                {`You will get ${utils.formatEther(
-                  removeCD,
-                )} Crypto Dev Tokens and ${utils.formatEther(removeEther)} ETH`}
+                {/* Convert the BigNumber to string using the formatEther function from ethers.js */}
+                {`You will get ${utils.formatEther(removeCD)} Crypto
+              Dev Tokens and ${utils.formatEther(removeEther)} Eth`}
               </div>
               <button
-                className={styles.button}
-                onClick={() => _removeLiquidity()}
+                className={styles.button1}
+                onClick={_removeLiquidity}
               >
                 Remove
               </button>
             </div>
           </div>
-        </>
+        </div>
       );
     } else {
       return (
-        <>
+        <div>
           <input
             type="number"
             placeholder="Amount"
             onChange={async (e) => {
               setSwapAmount(e.target.value || "");
+              // Calculate the amount of tokens user would receive after the swap
               await _getAmountOfTokensReceivedFromSwap(e.target.value || "0");
             }}
             className={styles.input}
@@ -336,8 +345,10 @@ export default function Home() {
           <select
             className={styles.select}
             name="dropdown"
+            id="dropdown"
             onChange={async () => {
               setEthSelected(!ethSelected);
+              // Initialize the values back to zero
               await _getAmountOfTokensReceivedFromSwap(0);
               setSwapAmount("");
             }}
@@ -347,6 +358,7 @@ export default function Home() {
           </select>
           <br />
           <div className={styles.inputDiv}>
+            {/* Convert the BigNumber to string using the formatEther function from ethers.js */}
             {ethSelected
               ? `You will get ${utils.formatEther(tokenToBeReceivedAfterSwap)} Crypto Dev Tokens`
               : `You will get ${utils.formatEther(tokenToBeReceivedAfterSwap)} Eth`}
@@ -357,7 +369,7 @@ export default function Home() {
           >
             Swap
           </button>
-        </>
+        </div>
       );
     }
   };
